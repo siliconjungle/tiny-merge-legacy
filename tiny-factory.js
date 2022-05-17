4189fdd4630a99aaa8ab399949505c9f0f9d1e9a
James Addison <siliconjungle.io@gmail.com>
2022-05-17 13:30:04 +1000

    add a factory for creating crdts

---
+++ tiny-factory.js
@@ -0,0 +1,15 @@
+const CRDT_TYPES = {
+  SEQUENCER: 'sequencer',
+  KEYSTORE: 'keystore',
+}
+
+export const createCrdt = (type) => {
+  switch (type) {
+    case CRDT_TYPES.SEQUENCER:
+      return createSequencer()
+    case CRDT_TYPES.KEYSTORE:
+      return createKeystore()
+    default:
+      throw new Error(`Unknown CRDT type: ${type}`)
+  }
+}
