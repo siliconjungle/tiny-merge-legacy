161a88f3b4e500e4144aa28a8486d573d700add2
James Addison <siliconjungle.io@gmail.com>
2022-05-19 17:39:12 +1000

    add helper functions for collections and documents

--- collection.js
+++ collection.js
@@ -1,8 +1,14 @@
 import * as keystore from './keystore.js'
+import { setCollectionByKey } from './root-helper'
 
 export const create = (ram, definition, userId) => {
   return {
     definition,
     documents: keystore.create(ram, {}, userId),
   }
+}
+
+export const set = (ram, root, key, definition, userId, version = 0) => {
+  const collection = create(ram, definition, userId)
+  return setCollectionByKey(ram, root, key, collection, userId, version)
 }
--- document.js
+++ document.js
@@ -1,7 +1,13 @@
 import * as keystore from './keystore.js'
+import { setDocumentByKey } from './root-helper'
 
 export const create = (ram, data, userId) => {
   return {
     data: keystore.create(ram, data, userId),
   }
+}
+
+export const set = (ram, collection, key, data, userId, version = 0) => {
+  const document = create(ram, data, userId)
+  return setDocumentByKey(ram, collection, key, document, userId, version)
 }
