263eb0d93a4b3b40e0b6a01337e4804dd59823ee
siliconjungle <siliconjungle.io@gmail.com>
2022-05-26 09:45:12 +1000

    remove tiny pairs - bad idea

--- ot-to-crdt.js
+++
@@ -1,8 +0,0 @@
-import { createRandomId } from './utils.js'
-
-const SERVER_ID = createRandomId()
-
-const convertOtOpToCrdtOp = (op) => {
-  const { address, value, parentVersion, userVersion, userId } = op
-  return { address, value, serverVersion: parentVersion + 1, serverId: SERVER_ID, userVersion, userId }
-}
